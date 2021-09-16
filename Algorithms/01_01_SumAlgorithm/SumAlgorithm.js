e72217fa67ddd7ddabcf62788e60414b94888e39
Reload0213 <85924448+goodbye20@users.noreply.github.com>
2021-09-17 00:33:15 +0900

    210917 lesson and quiz

---
+++ Algorithms/01_01_SumAlgorithm/SumAlgorithm.js
@@ -0,0 +1,10 @@
+//[?] n명의 점수 중에서 80점 이상인 점수의 합계를 구하라
+
+//[1] Input(입력): n명의 점수
+var scores = [ 100, 75, 50, 37, 90, 95 ];
+
+const up80 = {
+}
+    for (let index = 0; index < scores.length; index++) {
+        console.log(scores[index])
+    }
