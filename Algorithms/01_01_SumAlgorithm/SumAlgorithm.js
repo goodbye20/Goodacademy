d363c70d189e94aa18160afd91bc7a043c961ba7
Reload0213 <85924448+goodbye20@users.noreply.github.com>
2021-09-17 15:07:41 +0900

    210917 morning lesson

--- Algorithms/01_01_SumAlgorithm/SumAlgorithm.js
+++ Algorithms/01_01_SumAlgorithm/SumAlgorithm.js
@@ -3,8 +3,3 @@
 //[1] Input(입력): n명의 점수
 var scores = [ 100, 75, 50, 37, 90, 95 ];
 
-const up80 = {
-}
-    for (let index = 0; index < scores.length; index++) {
-        console.log(scores[index])
-    }
